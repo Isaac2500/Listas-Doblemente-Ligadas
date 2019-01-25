45ccd46580cc9eeb80bd9d52ed919b331021eaff
Isaac2500 <ICanche83@gmail.com>
2019-01-25 08:26:26 -0600

    Practica en proceso

---
+++ Ejercicios/Practica Polinomios/ListaPolinomios.java
@@ -0,0 +1,19 @@
+
+public class ListaPolinomios{
+
+    private Nodo head;
+    private int size = 0;
+
+    public ListaPolinomios(){
+        this.head = null;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+
+
+
+}
