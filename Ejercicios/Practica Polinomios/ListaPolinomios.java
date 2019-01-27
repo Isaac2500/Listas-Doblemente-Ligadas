6d6ab274c647174c7ee56305ce29379d29b9c7bc
Isaac2500 <ICanche83@gmail.com>
2019-01-27 15:12:03 -0600

    Implementado metodo insertar, aun con fallos

--- Ejercicios/Practica Polinomios/ListaPolinomios.java
+++ Ejercicios/Practica Polinomios/ListaPolinomios.java
@@ -9,11 +9,106 @@ public class ListaPolinomios{
     }
 
     public static void main(String[] args) {
-        
+        ListaPolinomios Ls = new ListaPolinomios();
+
+        Ls.insertarTermino(new Nodo(4,2));
+        Ls.imprimirPolinomio();
     }
 
-    
+    public boolean encontrarSimilitudes(Nodo xNodo){
+        
+        Nodo temp = head;
+        boolean band = false;
 
+        if(temp.obtenerSiguiente() == null){
+            return false;
+        }else{
+            while(temp.obtenerSiguiente() != null || band == false){
+            
+                if (temp.obtenerExponente() != xNodo.obtenerExponente()) {
+    
+                    temp.establecerCoeficiente(xNodo.obtenerCoeficiente() + temp.obtenerCoeficiente());
+                    band = true;
+    
+                } else {
+                    temp = temp.obtenerSiguiente();    
+                }
+    
+            }
+
+            if(band == true){
+                return true;
+            }else{
+                return false;
+            }
+        }     
+    }
+
+    public void insertarTermino(Nodo xNodo){
+        
+        if(this.encontrarSimilitudes(xNodo) != true){
+            Nodo temp = head;
+            
+            if(temp.obtenerSiguiente() == null){
+                
+                temp.enlazarSiguiente(xNodo);
+                xNodo.enlazarAnterior(temp);
+                size++;
+
+            }else{
+
+                int band = 0;
+                while (temp.obtenerSiguiente() != null || band == 0) {
+                    
+                    if(temp.obtenerExponente()<xNodo.obtenerExponente()){
+                        band = 1;
+                    }else{
+                        temp = temp.obtenerSiguiente();
+                    }
+                }
+
+               Nodo temp2 = head;
+
+               while(temp2.obtenerSiguiente()!=temp){
+                    temp2 = temp2.obtenerSiguiente();    
+               }
+
+               xNodo.enlazarSiguiente(temp);
+               xNodo.enlazarAnterior(temp2);
+               temp.enlazarAnterior(xNodo);
+               temp2.enlazarSiguiente(xNodo);
+
+               size++;
+            }
+        }
+    }
+
+/*    public void ordenarTermino(Nodo xNode){
+        
+        Nodo temp = head;
+        while(temp.obtenerSiguiente() != null || temp.obtenerExponente()<xNode.obtenerExponente()){
+            
+        }
+
+    }
+*/
+    public boolean listaVacia(){
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public void imprimirPolinomio(){
+        Nodo temp = head;
+        while(temp.obtenerSiguiente() != null){
+            temp = temp.obtenerSiguiente();
+            System.out.println(temp.obtenerCoeficiente() + "x^" + temp.obtenerExponente() +" + ");
+        }
+
+    }
 
 
 }
